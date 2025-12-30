59ab422baed6e075556464fc8b35cae62d25c8bf
0xmugetso <0xmugetso@gmail.com>
2025-12-30 18:28:54 +0330

    chore: add farcaster manifest redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b1fc1-2e5f-fcd9-451d-7faac80f1140',
+        permanent: false, // 307 Temporary Redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
